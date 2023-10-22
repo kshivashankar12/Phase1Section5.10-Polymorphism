308bd92b281f35d534b628bc23d51657a019f3dc
Kshivashankar12 <kshivashankargowda1999@gmail.com>
2023-10-22 16:05:23 +0530

    Add project files.

---
+++ Phase1Section5.10 Polymorphism/BasicGradeCard.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Phase1Section5._10_Polymorphism
+{
+    public class BasicGradeCard
+    {
+        protected int totalMarks;
+
+        public BasicGradeCard()
+        {
+            totalMarks = 0;
+        }
+
+        public virtual int getTotalMarks()
+        {
+            return totalMarks;
+        }
+    }
+}
+
